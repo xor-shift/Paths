dd9a703dd6fb070211bc0e02091b45530fa68ff9
Daswf852 <daswf852@protonmail.com>
2021-09-12 09:59:50 +0300

    WP fix, cleanup & speedup

--- lib/utils/include/utils/workerPool.hpp
+++ lib/utils/include/utils/workerPool.hpp
@@ -9,82 +9,6 @@
 #include "bufChan.hpp"
 #include "waitGroup.hpp"
 
-/*
-template<typename Fn, typename WorkItem, typename PromisedType = void>
-class WorkerPoolFuture {
-    typedef std::pair<WorkItem, std::promise<PromisedType>> ChanType;
-
-  public:
-    explicit WorkerPoolFuture(Fn &&fn, size_t bufSize)
-      : fn(fn)
-        , workItemChan(bufSize) {}
-
-    std::future<PromisedType> QueueWork(WorkItem item) {
-        ChanType workItem{
-          std::move(item),
-          std::promise<PromisedType>{},
-        };
-
-        auto future = workItem.second.get_future();
-
-        if (!workItemChan.EmplaceBack(std::move(workItem))) return {};
-
-        return future;
-    }
-
-    void Work(size_t nThreads = 1) {
-        std::vector<std::thread> workerThreads(nThreads);
-
-        for (auto &thread : workerThreads) {
-            thread = std::thread([this] {
-                for (;;) {
-                    auto workOpt = workItemChan.Get();
-                    if (!workOpt) break;
-
-                    auto &&[work, future] = *workOpt;
-
-                    if constexpr (std::is_same_v<PromisedType, void>) {
-                        fn(std::forward<WorkItem>(work));
-                        future.set_value();
-                    } else {
-                        auto res = fn(std::forward<WorkItem>(work));
-                        future.set_value(res);
-                    }
-                }
-            });
-        }
-
-        for (auto &thread : workerThreads) {
-            if (thread.joinable())
-                thread.join();
-        }
-    }
-
-    void Close() {
-        workItemChan.Close();
-    }
-
-    template<typename Callable>
-    std::vector<std::future<PromisedType>> SplitWork(size_t workSize, size_t workDivide, Callable &&wiGen) requires(std::is_same_v<std::invoke_result_t<Callable, size_t, size_t>, WorkItem>) {
-        const size_t nSegments = workSize / workDivide;
-        const size_t excessSegments = workSize & workDivide;
-
-        std::vector<std::future<PromisedType>> futures(nSegments);
-
-        for (size_t i = 0; i < nSegments; i++) {
-            const size_t start = i * workDivide;
-            const size_t end = start + workDivide + ((i + 1 == nSegments) ? excessSegments : 0);
-            futures.at(i) = std::move(QueueWork(wiGen(start, end)));
-        }
-
-        return futures;
-    }
-
-  private:
-    Fn fn;
-    BufChan<ChanType> workItemChan;
-};*/
-
 /**
  * A WaitGroup based worker pool
  * @tparam Fn The callback to call for working
@@ -108,24 +32,30 @@ class WorkerPoolWG {
     }
 
     void Work(size_t nThreads = 1) {
-        std::vector<std::thread> workerThreads(nThreads);
+        auto lambda = [this] {
+            for (;;) {
+                auto workOpt = workItemChan.Get();
+                if (!workOpt) break;
 
-        for (auto &thread : workerThreads) {
-            thread = std::thread([this] {
-                for (;;) {
-                    auto workOpt = workItemChan.Get();
-                    if (!workOpt) break;
+                WorkItem work = *workOpt;
+                std::invoke(fn, std::move(work));
+                wg.Done();
+            }
+        };
 
-                    auto work = std::move(*workOpt);
-                    fn(std::move(work));
-                    wg.Done();
-                }
-            });
-        }
+        if (nThreads == 1) {
+            lambda();
+        } else {
+            std::vector<std::thread> workerThreads(nThreads);
 
-        for (auto &thread : workerThreads) {
-            if (thread.joinable())
-                thread.join();
+            for (auto &thread : workerThreads) {
+                thread = std::thread(lambda);
+            }
+
+            for (auto &thread : workerThreads) {
+                if (thread.joinable())
+                    thread.join();
+            }
         }
     }
 
@@ -136,7 +66,7 @@ class WorkerPoolWG {
     template<typename Callable>
     void SplitWork(size_t workSize, size_t workDivide, Callable &&wiGen) requires(std::is_same_v<std::invoke_result_t<Callable, size_t, size_t>, WorkItem>) {
         const size_t nSegments = workSize / workDivide;
-        const size_t excessSegments = workSize & workDivide;
+        const size_t excessSegments = workSize % workDivide;
 
         WGDelta(nSegments);
 
