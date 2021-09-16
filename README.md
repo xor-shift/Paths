f33dfbc3fba7489f1e86ca8981c20c852abddf65
Daswf852 <daswf852@protonmail.com>
2021-09-16 20:02:41 +0300

    tweaks

--- README.md
+++ README.md
@@ -5,9 +5,27 @@ A path/ray tracer (no path tracing as of now)
 ## Libraries needed
 
 - [{fmt}](https://github.com/fmtlib/fmt), Used for text output
-- [SFML](https://github.com/SFML/sfml), Used to display rendered images to the screen, might switch to something lighter that gets this job done later on
-- ImGui, Used for UI alongside:
-- ImGui-SFML
+- [LodePNG](https://github.com/lvandeve/lodepng), Used for image output
+- [N. Lohmann's JSON library](https://github.com/nlohmann/json), Used for scene configurations and resume metadata
+- [GCEM](https://github.com/kthohr/gcem), I think I used it somewhere?
+
+{fmt} and json are managed by conan and lodepng is a submodule, you need to get fmt from your favourite package manager.
+
+## Usage
+
+- Compile the project
+- Check out `conf.example.json` for an example camera & scene configuration file
+- Run the project like so: `./a.out conf.json`
+
+Notes:
+
+- Interrupting the program will make it gracefully quit after saving resume files (this can be used to convert a resume binary to a png, heh)
+- Loading of resume files does not run checks for the scene the resume file was made on, a single check for the image dimensions is made
+
+## Eye candy
+
+10k samples, 1280x720, a few thousand triangles and 3 disc lights, no explicit light sampling  
+![render from commit 5319a06 or around that one](https://github.com/xor-shift/Paths/blob/master/example%20render/out.png?raw=true)
 
 ## Project layout
 
@@ -16,12 +34,12 @@ TODO: add layout
 ## To-Do
 
 - [ ] Better documentation
-- [ ] Separation of SFML components from lib/gfx into its own sub folder somewhere
-- [ ] Expression templates for linear algebra
+- [x] Expression templates for linear algebra
     - [x] Vectors and vector operations
-    - [ ] Matrices and matrix operations
-    - [ ] Vector matrix operations
+    - [x] Matrices and matrix operations
+    - [x] Vector matrix operations
 - [x] Preview mode (simplified Whitted)
-- [ ] Non-preview mode (actual MC path tracing)
+- [x] Non-preview mode (actual MC path tracing)
     - [ ] MLT for PT (very late goal)
-    - [ ] Volume interactions to some extent (this is also low priority)
+    - [ ] Volume interactions to some extent (this is also low priority, unfeasible to do without MLT)
+- [ ] Use the GPU to some extent (probably infeasible at this point, would need a complete port)
--- paths/main.cpp
+++ paths/main.cpp
@@ -1,3 +1,4 @@
+#include <csignal>
 #include <fmt/format.h>
 
 #include <gfx/camera/filter.hpp>
@@ -8,27 +9,16 @@
 void PrintHelp(const char *argv0) {
     fmt::print(
       "Usage:    ./{0} [camera setup file] <resume file>\n"
-      "Examples: ./{0} conf.json\n"
-      "          ./{0} conf.json resume.json", argv0);
+      "Examples: ./{0} conf.json\n", argv0);
+      //"          ./{0} conf.json resume.json", argv0);
 }
 
 template<Gfx::Concepts::BasicFilterUnary T>
 void Output(const std::string &filename, const Gfx::Image &image, const T &filter) {
-    //const size_t consoleWidth = 4 * 5;
-    //const size_t consoleHeight = 3 * 5;
-
     std::vector<unsigned char> imageData(image.Width() * image.Height() * 4);
     for (size_t y = 0; y < image.Height(); y++) {
         for (size_t x = 0; x < image.Width(); x++) {
             const auto color = filter(image.At(x, y));
-
-            /*if (y % consoleWidth == 0 && x % consoleWidth == 0) {
-                if (Math::Magnitude(color) > 8) {
-                    fmt::print("#");
-                } else {
-                    fmt::print(".");
-                }
-            }*/
 
             const auto idx = (x + y * image.Width()) * 4;
             imageData[idx + 0] = static_cast<uint8_t>(color[0]);
@@ -36,16 +26,22 @@ void Output(const std::string &filename, const Gfx::Image &image, const T &filte
             imageData[idx + 2] = static_cast<uint8_t>(color[2]);
             imageData[idx + 3] = 255;
         }
-        //if (y % consoleWidth == 0) fmt::print("\n");
     }
 
     lodepng::encode(filename, imageData, image.Width(), image.Height(), LCT_RGBA, 8);
 }
 
+static volatile std::sig_atomic_t sigVar = 0;
+
+void SIGHandler(int signum) { sigVar = 1; }
+
 int main(int argc, char *const *argv) {
+    std::signal(SIGINT, SIGHandler);
+    std::signal(SIGTERM, SIGHandler);
+
     PreparedCamera camera;
 
-    if (argc < 2 || argc > 3) {
+    if (argc != 2) {
         PrintHelp(argv[0]);
         return 1;
     } else {
@@ -57,10 +53,13 @@ int main(int argc, char *const *argv) {
 
         camera.integrator->DoRender();
 
+        bool interrupted = sigVar == 1;
+
         bool output = false;
         output |= (camera.outConfig.outputEvery != 0) &&
                   ((i + 1) % camera.outConfig.outputEvery == 0);
         output |= i + 1 == camera.outConfig.nSamples;
+        output |= interrupted;
 
         if (output) {
             const auto &image = camera.integrator->GetRender();
@@ -70,10 +69,12 @@ int main(int argc, char *const *argv) {
               Gfx::Filters::Basic::Oper([](Gfx::RGBSpectrum c) { return c * 255.; }),
             });
         }
+
+        if (interrupted) break;
     }
 
-    auto [image, nSamples] = camera.integrator->GetBackbuffer();
-
+    fmt::print("Saving resume to \"{}\"", camera.outConfig.resumeFileName);
+    auto[image, nSamples] = camera.integrator->GetBackbuffer();
     SaveResume(camera.outConfig.resumeFileName, image, nSamples);
 
     return 0;
